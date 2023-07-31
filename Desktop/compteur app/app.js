01137740944bc65a1d881d09e17fb070ec1a2530
djo kangoma <djokangoma@gamil.com>
2023-07-31 16:51:45 +0100

    mes codes sources

---
+++ Desktop/compteur app/app.js
@@ -0,0 +1,24 @@
+const buttonRemove=document.getElementById("btn-remove");
+const buttonAdd=document.getElementById("btn-add");
+const buttonReset=document.getElementById("btn-reset")
+const compteur=document.getElementById("compteur");
+
+function compteurAdd(){
+    console.log("buttonAdd");
+    const nombre=Number(compteur.innerHTML);
+    compteur.innerHTML=`${nombre + 1}`
+
+}
+
+function compteuremove(){
+    const nombre=Number(compteur.innerHTML);
+    if(nombre<0)
+    console.log("buttonRemove")
+}
+function reset(){
+    compteur.innerHTML='0';
+}
+
+buttonAdd.addEventListener("click",compteurAdd);
+buttonRemove.addEventListener("click",compteuremove);
+
