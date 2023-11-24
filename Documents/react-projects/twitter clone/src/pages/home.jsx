362a41a87a64aab82d8f54c0dc7b17e70d974f21
djo kangoma <djokangoma@gamil.com>
2023-11-24 16:59:49 +0100

    create style page

--- Documents/react-projects/twitter clone/src/pages/home.jsx
+++ Documents/react-projects/twitter clone/src/pages/home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from './../components/header';
+import TweetEditor from './../components/tweet-editor';
+import Tweets from './../components/tweets';
 
 function Home() {
   return (
     <main className="timeline">
-      <h1>Timeline here</h1>
+      <Header />
+      <TweetEditor />
+      <Tweets />
+
     </main>
   );
 }
